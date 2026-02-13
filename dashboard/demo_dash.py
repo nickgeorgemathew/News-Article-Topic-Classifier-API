4c4bdda829840c13a25fc076bc3069b7656ccc57
nickgeorgemathew <148985027+nickgeorgemathew@users.noreply.github.com>
2026-02-13 13:13:13 +0530

    Docker update ,demo

---
+++ dashboard/demo_dash.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import requests
+
+
+
+
+st.title("News article topic classifier Demo")
+
+
+Text=st.text_input(label="Enter the news article text",value="",type="default") 
+
+with st.spinner(text="predicting topic.......",width="content"):
+    if st.button("predict"):
+    
+        payload={       
+    "text":Text
+ 
+    }
+        res=requests.post("http://api:8000/predict",json=payload)
+        st.write("Status code:", res.status_code)
+        try:
+            st.json(res.json())
+        except Exception as e:
+            st.write("Response is not JSON:")
+            st.text(res.text)
+       
